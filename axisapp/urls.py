4a04f88a0c8129f8a356fb2d8c8c0a5686d4b6f6
Dairon Medina <dairon.medina@gmail.com>
2015-01-13 22:30:53 -0500

    Added jinja setup and basic templates

--- axisapp/urls.py
+++ axisapp/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django_jinja import views
+from django.conf import settings
+from django.views.generic import RedirectView
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,3 +12,13 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if not settings.USE_S3:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler403 = views.PermissionDenied.as_view()
+handler404 = views.PageNotFound.as_view()
+handler500 = views.ServerError.as_view()
