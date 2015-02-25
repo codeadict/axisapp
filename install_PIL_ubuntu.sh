04ac5e794a437b754f7eccbb2fb740d30a8eff3f
Miguel Albalat Aguila <mio.peaceandlove@gmail.com>
2015-02-24 22:43:58 -0500

    Adding install_PIL_ubuntu.sh to install PIL into ubuntu ,a pain in the ass

---
+++ install_PIL_ubuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Install libraries PIL needs
+sudo apt-get -y install python-dev libjpeg-dev libfreetype6-dev zlib1g-dev
+
+#Create the links
+sudo ln -s /usr/lib/`uname -i`-linux-gnu/libfreetype.so /usr/lib/
+sudo ln -s /usr/lib/`uname -i`-linux-gnu/libjpeg.so /usr/lib/
+sudo ln -s /usr/lib/`uname -i`-linux-gnu/libz.so /usr/lib/
+
+#Install PIL
+pip install PIL --allow-external PIL --allow-unverified PIL
+
