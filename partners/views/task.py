3e921b101850f81a3fc3b232367e9efbd9d83ffe
Miguel Albalat Aguila <mio.peaceandlove@gmail.com>
2015-03-07 00:21:00 -0500

    Using celery task.py file

---
+++ partners/views/task.py
@@ -0,0 +1,74 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import connection
+from celery import task
+
+from censo.models import PresalesDistribution, Cliente
+from base.models import Area
+
+
+@task
+def get_context_hull_by_areas(area_id, clients_amount):
+    """
+    :param area_id:
+    :param clients_amount:
+    :return: void
+    """
+    #Get the area if exist
+    try:
+        area = Area.objects.get(id=area_id)
+    except ObjectDoesNotExist:
+        return
+
+    #Erase all the previous data about this area
+    presales_old_list = PresalesDistribution.objects.filter(polygon__within=area.poligono)
+    presales_old_list.delete()
+
+    #Create cursor for the kmeans
+    cursor = connection.cursor()
+
+    clients = Cliente.objects.filter(coordenadas__within=area.poligono)
+
+    #We don't need this, thanks to GeoModel.. ;)
+    """cursor.execute('(SELECT censo_cliente.* '
+                   'FROM censo_cliente, base_area '
+                   'WHERE ST_Contains(base_area.poligono, censo_cliente.geom) '
+                   'AND base_area.id = %i)', [area.id])
+    clients_per_area = cursor.fetchall()"""
+
+    areas_amount = 0
+
+    if clients:
+        try:
+            #We need to split the number of client to get the specific amount of areas
+            areas_amount = clients.count()/clients_amount
+        except ZeroDivisionError:
+            # We need to check if the areas_amount is under 0
+            # If is it, just set it to 1 (default)
+            areas_amount = 1
+        else:
+            if areas_amount < 0:
+                areas_amount = 1
+
+    #Prepare the query
+    cursor.execute('SELECT kmeans, count(*), ST_ConvexHull(ST_Collect(geom)) AS geom '
+                   'FROM (SELECT kmeans(ARRAY[ST_X(geom), ST_Y(geom)], %s) OVER (), geom '
+                   'FROM censo_cliente WHERE ST_X(geom) IS NOT NULL AND ST_Y(geom) IS NOT NULL ) '
+                   'AS ksub GROUP BY kmeans ORDER BY kmeans;', [areas_amount])
+
+    #Fetch into list all the kmeans as (id, count of point into, geo)
+    result = cursor.fetchall()
+
+    for convex_hull_area in result:
+        #Create teh object
+        ps_dist_obj = PresalesDistribution()
+        # Save the convex hull polygon
+        ps_dist_obj.polygon = convex_hull_area[2]
+        #Save possible name as Area_name+#Subarea
+        ps_dist_obj.name = str(area.nombre) + str(convex_hull_area[0])
+        # Get he clients inside the convex hull
+        clients_into_area = Cliente.objects.filter(coordenadas__within=convex_hull_area[2])
+        # We need to save it first, to save the clients later
+        ps_dist_obj.save()
+        #Save them into the clients
+        ps_dist_obj.clients.add(clients_into_area)
+        ps_dist_obj.save()
