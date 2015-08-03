393f30da366c9faada8c063e975e3d9037b806a0
Miguel Albalat <yo@localhost.localdomain>
2015-08-02 20:54:35 -0500

    Adding censo_clientes/localsettings.py

--- censo_clientes/localsettings.py
+++ censo_clientes/localsettings.py
@@ -3,10 +3,24 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'censo3',
-        'USER': 'codeadict',
-        'PASSWORD': 'unixrocks2012',
+        'NAME': 'axisapp2',
+        'USER': 'postgres',
+        'PASSWORD': '1 m3 m1n3',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+	'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+    'PAGINATE_BY': 10
+}
+
